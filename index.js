312869b588951824682fd8fe22cf525bc75c9ab0
Manuel Claeys Bouuaert <manuel.claeys.b@gmail.com>
2016-06-10 21:52:10 +0200

    try

--- index.js
+++ index.js
@@ -4,11 +4,5 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-var myStyle = {
-    "color": "#cec4bc", // medium? brown
-    "weight": 1,  // stroke weight in pixels
-    "opacity": 0.65
-};
-
-var geojsonLayer = new L.GeoJSON.AJAX('myTrace.geojson', {style: myStyle});
+var geojsonLayer = new L.GeoJSON.AJAX('myTrace.geojson');
 geojsonLayer.addTo(map);
