97922e5fdaa2b045451a108b4b600b0cb60b67d0
Manuel Claeys Bouuaert <manuel.claeys.b@gmail.com>
2016-06-12 18:05:57 +0200

    all in html

--- index.js
+++
@@ -1,8 +0,0 @@
-var map = L.map('map');
-
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-var myBuffer = new L.GeoJSON.AJAX('myBuffer.geojson');
-myBuffer.addTo(map);
