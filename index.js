b6c53a0dbc2e0747bfc52e927a76df23e290cb0a
Manuel Claeys Bouuaert <manuel.claeys.b@gmail.com>
2016-06-12 17:44:05 +0200

    buffer

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+var buffer = require('turf-buffer')
+
 var map = L.map('map').setView([51, 3.5], 5);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-var geojsonLayer = new L.GeoJSON.AJAX('myTrace.geojson');
-geojsonLayer.addTo(map);
+var myTrace = new L.GeoJSON.AJAX('myTrace.geojson');
+var myBuffer = buffer(myTrace);
+myBuffer.addTo(map);
